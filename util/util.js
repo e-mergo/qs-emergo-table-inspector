33e0210a0a3456a3315b5c462f4152d56ce08e7d
Laurens Offereins <lmoffereins@gmail.com>
2023-07-19 10:39:17 +0200

    Update E-mergo Util library to version 20230707

--- util/util.js
+++ util/util.js
@@ -1,7 +1,7 @@
 /**
  * E-mergo Utility library
  *
- * @version 20230307
+ * @version 20230707
  *
  * @package E-mergo
  *
@@ -266,6 +266,40 @@ define([
 	},
 
 	/**
+	 * Return whether the app runs in a Qlik Cloud context
+	 *
+	 * Determines context by checking app model attributes:
+	 * - layout._resourcetype
+	 * - layout.stream (not in Qlik Cloud)
+	 * - properties.ownerId
+	 *
+	 * @return {Boolean} Is the context Qlik Cloud?
+	 */
+	isQlikCloud = app.model.enigmaModel.layout.hasOwnProperty("_resourcetype")
+		&& ! app.model.enigmaModel.layout.hasOwnProperty("stream")
+		&& app.model.enigmaModel.properties.hasOwnProperty("ownerId"),
+
+	/**
+	 * Return whether the app runs in a Qlik Client Managed context
+	 *
+	 * Determines context by checking app model attributes:
+	 * - layout.stream
+	 *
+	 * @return {Boolean} Is the context Qlik Client Managed?
+	 */
+	isQlikSenseClientManaged = app.model.enigmaModel.layout.hasOwnProperty("stream"),
+
+	/**
+	 * Return whether the app runs in a Qlik Sense Desktop context
+	 *
+	 * Determines context by checking app model attributes:
+	 * - layout.create (not in Qlik Sense Desktop)
+	 *
+	 * @return {Boolean} Is the context Qlik Sense Desktop?
+	 */
+	isQlikSenseDesktop = app.model.enigmaModel.layout.hasOwnProperty("create"),
+
+	/**
 	 * Return a number from an expression's result
 	 *
 	 * @param  {String}  a Expression's result
@@ -423,39 +457,35 @@ define([
 	requireMarkdownMimetype = function() {
 		var dfd = $q.defer();
 
-		// Check desktop vs other environments
-		app.global.isPersonalMode().then( function( resp ) {
+		// Bail when not in a Client Managed environment
+		if (! util.isQlikSenseClientManaged) {
+			dfd.resolve(true);
+			return;
+		}
 
-			// Bail when in a Desktop environment
-			if (resp.qReturn) {
-				dfd.resolve(true);
-				return;
-			}
+		// Check whether the Markdown mimetype is registered
+		qlikRequest({
+			url: "/qrs/mimetype?filter=mime so 'markdown'"
+		}).then( function( resp ) {
 
-			// Check whether the Markdown mimetype is registered
-			qlikRequest({
-				url: "/qrs/mimetype?filter=mime so 'markdown'"
-			}).then( function( resp ) {
-
-				// Create Markdown mimetype when it's not registered
-				if (! resp.data.length) {
-					qlikRequest({
-						method: "POST",
-						url: "/qrs/mimetype",
-						data: {
-							additionalHeaders: null,
-							binary: false,
-							extensions: "md,markdown",
-							mime: "text/markdown"
-						}
-					}).then( function( resp ) {
-						dfd.resolve(true);
-					}).catch(dfd.reject);
-				} else {
+			// Create Markdown mimetype when it's not registered
+			if (! resp.data.length) {
+				qlikRequest({
+					method: "POST",
+					url: "/qrs/mimetype",
+					data: {
+						additionalHeaders: null,
+						binary: false,
+						extensions: "md,markdown",
+						mime: "text/markdown"
+					}
+				}).then( function( resp ) {
 					dfd.resolve(true);
-				}
-			}).catch(dfd.reject);
-		});
+				}).catch(dfd.reject);
+			} else {
+				dfd.resolve(true);
+			}
+		}).catch(dfd.reject);
 
 		return dfd.promise;
 	},
@@ -726,6 +756,9 @@ define([
 		createCache: createCache,
 		hexToRgb: hexToRgb,
 		isDarkColor: isDarkColor,
+		isQlikCloud: isQlikCloud,
+		isQlikSenseClientManaged: isQlikSenseClientManaged,
+		isQlikSenseDesktop: isQlikSenseDesktop,
 		numberFromExpression: numberFromExpression,
 		parseDynamicParams: parseDynamicParams,
 		qlikRequest: qlikRequest,
